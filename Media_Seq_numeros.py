a21c30f5d8c617c954e372b6866d709ee2b2c2a5
Mil de Paula <millenadepaula@gmail.com>
2022-07-05 19:48:52 -0300

    Media_ Seq. de números

---
+++ Media_Seq_numeros.py
@@ -0,0 +1,5 @@
+soma = float(input("Digite um número: "))
+for n in range(2, 6):
+    soma += float(input("Digite um número: "))
+    media = soma / n
+    print(f"A soma é: {soma} e a média é {media}")
